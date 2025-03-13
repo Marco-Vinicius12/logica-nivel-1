fe52f32ee4c0e7f4cbb44eafa53972d4f6ee9287
Marco-Vinicius12 <marco126498@gmail.com>
2025-03-13 19:55:16 -0300

    ex.04.py

---
+++ ex.04.py
@@ -0,0 +1,11 @@
+#4. Quadrado - Escrever um programa que imprima a seguinte figura:
+#XXXXX
+#X   X
+#X   X
+#X   X
+#XXXXX
+print ("XXXXX")
+print("x   x")
+print("x   x")
+print("x   x")
+print("XXXXX")
