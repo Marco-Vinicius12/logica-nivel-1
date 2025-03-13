06281738e3376e0e17c92dc74f7f3ef1db4b4bf9
Marco-Vinicius12 <marco126498@gmail.com>
2025-03-13 19:40:56 -0300

    ex-02.py

--- ex-02.py
+++ ex-02.py
@@ -1,3 +1,4 @@
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.#
 print("Marco Vinicius")
 print("Rio de Janeiro")
 print("Itaboraí")
