1cd98b2433827352f9f1e9700383a40dd43d40d8
Marco-Vinicius12 <marco126498@gmail.com>
2025-03-13 20:28:06 -0300

    ex-06.py

---
+++ ex-06.py
@@ -0,0 +1,8 @@
+#6. Menu - Elabore um programa que mostre o seguinte menu na tela:
+print("Cadastro de Cliente")
+print("0 - fim")
+print("1 - inclui")
+print("2 - Altera")
+print("3 - Exclui")
+print("4 - Consulta")
+print("Opção:")
